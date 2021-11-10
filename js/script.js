f2ddce011c88908717ef02ca5a5b08ac06e51eea
Igor <zaplatynskyy@gmail.com>
2021-11-10 18:17:38 +0100

    suggerimento mail per avviare il gioco

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // * Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 // * Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+alert('per entrare nel gioco inserisci la mail pippo@gmail.com')
 
 // Controllo email
 let button = document.querySelector('.row.mail .btn');
